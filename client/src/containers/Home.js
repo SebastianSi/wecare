baa2b9dc3e8b84fa07e12ac2394a4b9afc8bb518
sebastian.sirb <sebastian.sirb90@gmail.com>
2018-04-21 12:46:38 +0300

    some refactoring on the client

---
+++ client/src/containers/Home.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import Snackbar from 'material-ui/Snackbar';
+import MainTableContainer from './MainTableContainer';
+import '../css/App.css';
+
+class Home extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            employeesData: []
+        }
+    }
+
+    componentDidMount() {
+        this.getEmployeesData()
+    }
+
+
+    getEmployeesData = () => {
+
+        fetch('api/employee', {
+            method: 'get',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(res=>res.json())
+            .then( res => {
+                console.log(res)
+                this.setState({employeesData: res})
+            }).catch(function(err) {
+            console.log(err)
+        })
+    }
+
+    render() {
+
+        return (
+            <MuiThemeProvider>
+                <div className="App">
+                    <header className="App-header">
+                        <h1 className="App-title">Employees Management Platform</h1>
+                    </header>
+                    <p className="App-intro">
+                        Built with Java, Spring, React, and Material-UI.
+                    </p>
+                    <MainTableContainer
+                        getEmployees={this.getEmployeesData}
+                        employeesData={this.state.employeesData}
+                    />
+                    <div>
+                        {/*<Snackbar*/}
+                        {/*bodyStyle={{backgroundColor: 'green'}}*/}
+                        {/*autoHideDuration={5000}*/}
+                        {/*message={'Succesfully Registered!'}*/}
+                        {/*open={true}*/}
+                        {/*/>*/}
+                        {/*<Snackbar*/}
+                        {/*bodyStyle={{backgroundColor: 'red'}}*/}
+                        {/*autoHideDuration={5000}*/}
+                        {/*message={'An error occured when trying to register!'}*/}
+                        {/*open={true}*/}
+                        {/*/>*/}
+                    </div>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
+}
+
+export default Home;
