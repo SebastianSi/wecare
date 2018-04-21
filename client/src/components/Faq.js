a24978b24bc5fd3bf7ff7689bef48be27c5608b4
danutchindris <danutchindris@gmail.com>
2018-04-21 20:56:04 +0300

    add info on the faq page

--- client/src/components/Faq.js
+++ client/src/components/Faq.js
@@ -6,6 +6,54 @@ export default class Faq extends Component {
     return(
       <div>
         <h1>FAQ</h1>
+        <h2>Acte necesare la internare</h2>
+        <ul>
+            <li>buletin/CI</li>
+            <li>bilet de trimitere de la Medicul de familie</li>
+            <li>card de sănătate</li>
+            <li>adeverință de asigurat</li>
+        </ul>
+        <h2>Acte necesare intocmirii dosarului pentru incadrare in grad de handicap pentru copii</h2>
+        <ul>
+            <li>cerere tip</li>
+            <li>fisa de evaluare psihologica</li>
+            <li>fisa medicala sintetica</li>
+            <li>fisa psihopedagogica</li>
+            <li>certificate medical tip A5</li>
+            <li>ancheta sociala si anexa  acesteia (factori de mediu)</li>
+        </ul>
+        <div>
+            Alte documente necesare la dosar (declarație pe propria răspundere dată în fața asistentului social din primărie:
+            nume în clar, semnătura, numar de înregistrare de la registratură, data și ștampila primăriei, pentru părinții care
+            nu locuiesc cu forme legale la adresa din C.I- adresa reședinței; copie certificat de naştere, C.I. – copil, copie BI/CI
+            părinţi/reprezentant legal, copie a certificatului de căsătorie/divorţ/deces al părinţilor; (dacă este cazul)
+            copie sentinţă de divorţ, hotărâre/dispoziţie plasament (dacă este cazul) adeverinţă şcolară; copie a certificatului
+            de orientare şcolară şi profesională în vigoare, (dacă este în şcoală specială, sau beneficiază de profesor
+            de sprijin/itinerant);  copie a certificatului de încadrare în grad de handicap în vigoare, la reevaluare.
+        </div>
+        <h2>Acte necesare intocmirii dosarului pentru incadrare in grad de handicap pentru adulti</h2>
+        <ul>
+            <li>cerere tip</li>
+            <li>copia crtificatului de incadrare in grad de handicap pt cei care au mai fost la comisie</li>
+            <li>referat de la medicul specialist</li>
+            <li>scrisoare medicala de la medicul de familie</li>
+            <li>ancheta sociala de la primarie</li>
+            <li>bilete iesire spital, scrisori/retete medicale, rezultat CT, Ex. HISTOPATOLOGIC etc.</li>
+            <li>alte acte medicale mentionate pe certificatul de incadrare (in functie de diagnostic) in grad de handicap (ex. raport psihologic)</li>
+            <li>copie decizie pensie limita varsta/urmas/boala</li>
+            <li>copie cupon pensie</li>
+            <li>adeverinta venit</li>
+            <li>CI</li>
+        </ul>
+        <h2>Instrumente de lucru folosite de noi in managementul de caz</h2>
+        <ul>
+            <li>fisa de luare in evidenta</li>
+            <li>ancheta sociala</li>
+            <li>plan de interventie</li>
+            <li>raport de intrevedere</li>
+            <li>raport de interventie</li>
+            <li>fisa de inchidere caz</li>
+        </ul>
       </div>
     )
   }
