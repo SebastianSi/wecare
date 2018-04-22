dd7c3a44e67e241430b711b8546a6b1173e9995c
sebastian.sirb <sebastian.sirb90@gmail.com>
2018-04-22 11:38:31 +0300

    very unusable modal

--- client/src/components/EditPatientModal.js
+++ client/src/components/EditPatientModal.js
@@ -1,15 +1,52 @@
 import React from 'react';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
-
+import TextField from 'material-ui/TextField';
+import Constants from "../AppConstants";
+// import Divider from 'material-ui/Divider';
 
 export default class EditPatientModal extends React.Component {
     state = {
         open: false,
+        currPatientId: null,
+        cnp: '',
+        firstName: '',
+        lastName: '',
+        city: '',
+        county: '',
+        address: '',
+        email: '',
+        dueDate: '',
+        phone: ''
+
     };
 
     handleOpen = () => {
         this.setState({open: true});
+    };
+
+    handleCityChange = (event) => {
+        this.setState({
+            city: event.target.value
+        });
+    };
+
+    handleAddressChange = (event) => {
+        this.setState({
+            address: event.target.value
+        });
+    };
+
+    handlePhoneChange = (event) => {
+        this.setState({
+            phone: event.target.value
+        });
+    };
+
+    handleCnpChange = (event) => {
+        this.setState({
+            cnp: event.target.value
+        });
     };
 
     handleClose = () => {
@@ -17,39 +54,107 @@ export default class EditPatientModal extends React.Component {
     };
 
     handleRemove = () => {
-        this.props.removeEmployee()
         this.handleClose();
     };
 
     componentWillReceiveProps(nextProps) {
-      this.setState({open: nextProps.isOpen})
+        console.log('NEXTPROPS: ', nextProps)
+        this.setState({
+            open: nextProps.isOpen,
+            currPatientId: nextProps.patientId
+        })
+        let { patientId } = nextProps
+        fetch(`api/patients/${patientId}`, {
+        method: 'get',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+        }).then(res=>res.json())
+            .then( res => {
+                console.log('GetPatientByID: ', res)
+                this.setState({
+                    cnp: res.cnp,
+                    firstName: res.firstName,
+                    lastName: res.lastName,
+                    city: res.city,
+                    county: res.county,
+                    address: res.address,
+                    email: res.email,
+                    dueDate: res.dueDate,
+                    phone: res.phone
+                })
+            }).catch(function(err) {
+            console.log(err)
+        });
     }
 
     render() {
         const actions = [
             <FlatButton
                 label="Cancel"
-                primary={true}
+                primary={false}
                 onClick={this.handleClose}
             />,
             <FlatButton
-                label="Remove"
+                label="Submit"
                 primary={true}
                 onClick={this.handleRemove}
-            />,
+            />
         ];
 
         return (
             <div>
+
                 <Dialog
-                    title="Dialog With Actions"
+                    title="Detailed Patient Info"
                     actions={actions}
                     modal={true}
                     open={this.state.open}
+                    // bodyStyle={{maxHeight: '1000px !important'}}
+                    // contentStyle={{maxHeight: '1000px !important'}}
+                    // style={{maxHeight: '1000px !important'}}
+                    // actionsContainerStyle={{maxHeight: '1000px !important'}}
+                    // overlayStyle={{maxHeight: '1000px !important'}}
                 >
-                    Only actions can close this dialog.
+                    <TextField
+                        value={this.state.firstName + ' ' + this.state.lastName}
+                        floatingLabelText="Patient Name"
+                        // onChange={this.handleChange}
+                    />
+                    <br/>
+                    <TextField
+                        value={this.state.cnp}
+                        floatingLabelText="CNP"
+                        onChange={this.handleCnpChange}
+                    />
+                    <br/>
+                    <TextField
+                        value={this.state.city}
+                        floatingLabelText="City"
+                        onChange={this.handleCityChange}
+                    />
+                    <br/>
+                    <TextField
+                        value={this.state.address}
+                        floatingLabelText="Address"
+                        onChange={this.handleAddressChange}
+                    />
+                    <br/>
+                    <TextField
+                        value={this.state.phone}
+                        floatingLabelText="Phone"
+                        onChange={this.handlePhoneChange}
+                    />
+                    <br/>
+                    <TextField
+                        value={ new Date(this.state.dueDate).toDateString()}
+                        floatingLabelText="Due Date"
+                    />
+                    <br/>
                 </Dialog>
             </div>
         );
     }
 }
+
